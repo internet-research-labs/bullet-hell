3873aee6b163c76fda4ea567bd174cd2d35331a2
Matt 3 <not.mattowen@gmail.com>
2020-10-31 13:41:06 -0400

    :wolf: Pass hub correctly to websocket connect function

--- src/hub.rs
+++ src/hub.rs
@@ -1,31 +1,22 @@
-use std::convert::TryInto;
+// use std::convert::TryInto;
 
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-};
-
+#[derive(Default)]
 pub struct Hub {
-    curr: AtomicUsize
+    curr: i64,
 }
 
 impl Hub {
     pub fn new() -> Hub {
         Hub{
-            curr: AtomicUsize::new(1),
+            // curr: AtomicUsize::new(1),
+            curr: 1,
         }
     }
 
-    pub fn uuid(&self) -> i64 {
-        self.curr.fetch_add(1, Ordering::Relaxed)
-            .try_into().unwrap()
-    }
-}
-
-impl Clone for Hub {
-    fn clone(&self) -> Self {
-        let ret = Hub::new();
-        let i = self.curr.load(Ordering::Relaxed);
-        ret.curr.store(i, Ordering::Relaxed);
-        ret
+    pub fn uuid(&mut self) -> i64 {
+        self.curr += 1;
+        self.curr
+        // self.curr.fetch_add(1, Ordering::Relaxed)
+        //    .try_into().unwrap()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-// mod zone;
-// use zone::Pos;
-// use zone::Coordinate;
-
 mod zone;
 mod hub;
 
 // use tokio::sync::{mpsc, RwLock};
+use tokio::sync::RwLock;
+
+use std::sync::Arc;
+
 use warp::Filter;
 
-async fn connect(_ws: warp::ws::WebSocket, id: i64) {
-    println!("Connected... id = {}", id);
+async fn connect(_ws: warp::ws::WebSocket, hub: ArcHub) {
+    let id = hub.write().await.uuid();
+    println!("Connected... {}", id)
 }
+
+type ArcHub = Arc<RwLock<hub::Hub>>;
 
 #[tokio::main]
 async fn main() {
 
-    // NOTE: This seems pretty sketch...
-    // Hub shouldn't be cloned for every connection (???)
-    let zzz = hub::Hub::new();
-    let zzz = warp::any().map(move || zzz.uuid());
+    let hub = ArcHub::default();
 
     let index = warp::path::end()
         .and(warp::fs::file("www/static/index.html"));
@@ -28,9 +28,9 @@ async fn main() {
 
     let websocket = warp::path("ws")
         .and(warp::ws())
-        .and(zzz)
-        .map(|ws: warp::ws::Ws, uuid| {
-            ws.on_upgrade(move |socket| connect(socket, uuid))
+        .and(warp::any().map(move || hub.clone()))
+        .map(|ws: warp::ws::Ws, h| {
+            ws.on_upgrade(move |websocket| connect(websocket, h))
         });
 
     let statics = warp::path("static")
