e551fb0110a06a10265f798c66f1d8f39399f2a6
whatever <mattisbusiness@gmail.com>
2020-11-02 22:03:30 -0500

    :wolf: Clean-up warnings

--- src/fake.rs
+++ src/fake.rs
@@ -3,18 +3,6 @@ use std::collections::HashMap;
 use tokio::sync::mpsc;
 use tokio::sync::RwLock;
 
-pub type HubArcRef = Arc<RwLock<Hub>>;
-
-pub struct Hub {
-}
-
-impl Hub {
-    pub fn new() -> Hub {
-        Hub{}
-    }
-}
-
-
 pub struct Walkie {
     pub tx: mpsc::UnboundedSender<String>,
     pub rx: mpsc::UnboundedReceiver<String>,
@@ -22,8 +10,8 @@ pub struct Walkie {
 
 impl Walkie {
     pub fn gen() -> (Walkie, mpsc::UnboundedSender<String>, mpsc::UnboundedReceiver<String>) {
-        let (world_updates_tx, mut world_updates_rx) = mpsc::unbounded_channel();
-        let (player_updates_tx, mut player_updates_rx) = mpsc::unbounded_channel();
+        let (world_updates_tx, world_updates_rx) = mpsc::unbounded_channel();
+        let (player_updates_tx, player_updates_rx) = mpsc::unbounded_channel();
 
         let w = Walkie {
             rx: player_updates_rx,
@@ -38,6 +26,5 @@ impl Walkie {
     }
 }
 
-pub type UpdateReceiver = mpsc::UnboundedReceiver<Result<warp::ws::Message, warp::Error>>;
 pub type UpdateSender = mpsc::UnboundedSender<Result<warp::ws::Message, warp::Error>>;
 pub type Users = Arc<RwLock<HashMap<i64, UpdateSender>>>;
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,13 @@
-mod zone;
-mod hub;
 mod fake;
+mod zone;
 
-use rand::Rng;
 use tokio::sync::mpsc;
 use tokio::sync::RwLock;
 
 use futures::{FutureExt, StreamExt, join};
-use std::sync::Arc;
 
 use warp::Filter;
-use std::sync::Mutex;
 
-
-/// Handle user connected:
-/// 1. Register user with Hub
-/// 2. Setup rx for game updates
-/// 3. Setup tx for user inputs
-/// ...
-async fn connect(socket: warp::ws::WebSocket, h: hub::ArcHub) {
-
-    println!("1...");
-
-    let (user_ws_tx, mut user_ws_rx) = socket.split();
-
-    let (update_tx, update_rx) = mpsc::unbounded_channel();
-
-    let conn = h.write().await.new_conn(update_tx.clone());
-
-    let id = conn.id;
-
-    println!("2...");
-
-    tokio::task::spawn(update_rx.forward(user_ws_tx).map(move |result| {
-        if let Err(e) = result {
-            eprintln!("[{}] websocket send error: {}", id, e);
-        }
-    }));
-
-    println!("3...");
-
-    // TODO: Lift this out, but also explain it better
-    while let Some(result) = user_ws_rx.next().await {
-        println!(".... everything is cool");
-        let msg = match result {
-            Ok(msg) => {
-                let _msg = if let Ok(s) = msg.to_str() {
-                    // NOTE: This is the important part of this loop
-                    let m = warp::ws::Message::text(s);
-                    println!("????? {}", s);
-                    conn.tx.send(Ok(m));
-                } else {
-                    break;
-                };
-            },
-            Err(_) => {
-                break;
-            },
-        };
-    }
-
-    h.write().await.remove(id);
-    println!("Clossing connection: {}", id)
-}
 
 struct World {
     f: f64,
@@ -75,13 +20,13 @@ async fn updates(walkie: fake::Walkie, users: fake::Users) {
     let w = RwLock::new(World{f: 0.0});
 
     // Send to async loop
-    let tx = walkie.tx.clone();
+    let _tx = walkie.tx.clone();
 
     let fin = async {
         loop {
             let w = w.read().await;
 
-            for (i, tx) in users.write().await.iter() {
+            for (_, tx) in users.write().await.iter() {
                 tx.send(Ok(warp::ws::Message::text(w.f.to_string())));
             }
 
@@ -97,7 +42,7 @@ async fn updates(walkie: fake::Walkie, users: fake::Users) {
             let f = msg.parse::<f64>().ok().unwrap();
             let mut w = w.write().await;
             w.f += f;
-            println!("RX: {}", w.f);
+            println!("RX+{}: {}", f, w.f);
         }
     };
 
@@ -107,11 +52,16 @@ async fn updates(walkie: fake::Walkie, users: fake::Users) {
     );
 }
 
+/// Handle user connected:
+/// 1. Register user with Hub
+/// 2. Setup rx for game updates
+/// 3. Setup tx for user inputs
+/// ...
 async fn fake_connect(socket: warp::ws::WebSocket, to_game: mpsc::UnboundedSender<String>, users: fake::Users) {
 
     let (user_ws_tx, mut user_ws_rx) = socket.split();
 
-    let (update_tx, mut update_rx) = mpsc::unbounded_channel();
+    let (update_tx, update_rx) = mpsc::unbounded_channel();
 
     users.write().await.insert(0, update_tx.clone());
 
@@ -124,7 +74,7 @@ async fn fake_connect(socket: warp::ws::WebSocket, to_game: mpsc::UnboundedSende
 
     // Player -> World
     while let Some(result) = user_ws_rx.next().await {
-        let msg = match result {
+        let _ = match result {
             Ok(msg) => {
                 let _msg = if let Ok(s) = msg.to_str() {
                     to_game.send(s.to_string());
@@ -137,25 +87,19 @@ async fn fake_connect(socket: warp::ws::WebSocket, to_game: mpsc::UnboundedSende
             },
         };
     }
-
-
-    to_game.send("What!?".to_string());
 }
 
 #[tokio::main]
 async fn main() {
 
-    let (walkie, tx, rx) = fake::Walkie::gen();
+    let (walkie, tx, _rx) = fake::Walkie::gen();
 
     let users = fake::Users::default();
     let u = users.clone();
 
-    let update_world = tokio::task::spawn(async move {
+    tokio::task::spawn(async move {
         updates(walkie, u).await;
     });
-
-    let hub = Arc::new(RwLock::new(fake::Hub::new()));
-    let hub = hub::ArcHub::new(RwLock::new(hub::Hub::new()));
 
     let usr = warp::any().map(move || users.clone());
     let com = warp::any().map(move || tx.clone());
